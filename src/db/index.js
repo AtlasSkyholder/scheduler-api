9f1bb8ebd504dc75092fe8faf443c3a85522151b
morbi <the_morbidus@hotmail.com>
2020-04-24 19:40:09 -0400

    Updated database connection to work with postgresql on aws RDS

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,11 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT
 });
 
 client
